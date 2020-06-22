f9d762140e1aec261d1efbaeb2de400fe21edb42
Seratna <61306175+SeratnaBass@users.noreply.github.com>
2020-06-22 19:20:28 +0900

    Add files via upload

---
+++ 171/a.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<list>
+
+#define endl "\n"
+using namespace std;
+using ll = long long;
+const ll MOD = 1e9+7;
+const ll INF = 1e18;
+#define REP(i, n) for(int i = 0; i < n; i++)
+using Graph = vector<vector<int>>;
+
+int main(){
+    char a;
+    cin>>a;
+    if(a >= 'a') cout<<'a'<<endl;
+    else cout<<'A'<<endl;
+}
